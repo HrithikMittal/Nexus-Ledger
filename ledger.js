dbba750981e631291eef904483fd74f29a77ee44
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-02-09 20:14:39 +0530

    set env

--- ledger.js
+++ ledger.js
@@ -2,8 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var Details = require("app/models/cash");
-var Ledger = require("app/models/ledger");
+var Details = require("./app/models/cash");
+var Ledger = require("./app/models/ledger");
 var MongoClient = require("mongodb").MongoClient;
 
 
